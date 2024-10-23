5af2e12ec2c3fe8255665af07535a0db77b03b46
seokjin <tjrwls08088@naver.com>
2024-10-23 23:11:45 +0900

    feat: 선착순 이벤트 로직 구현 정리

--- src/main/java/com/system/fcfs/infra/CouponRepository.java
+++
@@ -1,7 +0,0 @@
-package com.system.fcfs.infra;
-
-import com.system.fcfs.domain.Coupon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CouponRepository extends JpaRepository<Coupon, Long> {
-}
