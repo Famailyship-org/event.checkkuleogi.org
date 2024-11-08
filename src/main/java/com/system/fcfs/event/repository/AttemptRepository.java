2752455f577cc3803c3070a9a6d6cc3a9154f603
seokjin <tjrwls08088@naver.com>
2024-11-08 14:35:45 +0900

    style: import 구문 삭제
    Resolves: #1

--- src/main/java/com/system/fcfs/event/repository/AttemptRepository.java
+++ src/main/java/com/system/fcfs/event/repository/AttemptRepository.java
@@ -1,12 +1,10 @@
 package com.system.fcfs.event.repository;
 
 import com.system.fcfs.event.domain.Attempt;
-import com.system.fcfs.event.domain.Winner;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 
 import java.util.List;
 import java.util.Optional;
